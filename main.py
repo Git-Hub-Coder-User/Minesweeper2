0548dbc81c19d9557aa8324f7901b5f90bb15a86
Git-Hub-Coder-user <elsa.schultheiss@ucas-edu.net>
2024-12-16 11:45:56 -0700

    I now have a bottom bar!

--- main.py
+++ main.py
@@ -11,12 +11,12 @@ from tile import(
 )
 
 pygame.init()
-screen = pygame.display.set_mode((800, 800))
+screen = pygame.display.set_mode((800, 850))
 clock = pygame.time.Clock()
 
-def generate_puzzle(): 
+def generate_puzzle(bombs): 
     background = Grid()
-    background.generate_bombs()
+    background.generate_bombs(bombs)
     background.convert_grid()
     return background
 
@@ -31,9 +31,7 @@ def check_win(background, flags):
     
     return returned
 
-def won_game(time):
-    #print(time)
-    font = pygame.font.SysFont(None, 84)
+def won_game(time, font):
     surface = font.render("You Won! ", True, (0, 225, 0))
     screen.blit(surface, (300, 100))
     surface = font.render(f"Time: {int(time / 1000)} seconds", True, (0, 225, 0))
@@ -50,8 +48,7 @@ def won_game(time):
                 if event.key == pygame.K_SPACE:
                     main()
 
-def lost_game():
-    font = pygame.font.SysFont(None, 84)
+def lost_game(font):
     surface = font.render("You Lost! ", True, (225, 0, 0))
     screen.blit(surface, (300, 100))
     surface = font.render("Press space to play again", True, (0, 105, 50))
@@ -67,12 +64,34 @@ def lost_game():
                 if event.key == pygame.K_SPACE:
                     main()
 
+def bottom_bar_flags(flags, screen):
+    font = pygame.font.SysFont(None, 36)
+    flag_num = font.render(f"{flags}", True, (225, 225, 225))
+    if flags == 16: 
+        flag = pygame.image.load("img/flag_img.png")
+        flag = pygame.transform.scale(flag, (40, 40))
+        screen.blit(flag, (200, 810))
+    pygame.draw.rect(screen, (0, 105, 0), (250, 815, 50, 50))
+    screen.blit(flag_num, (250, 815))
+
+def bottom_bar_time(time, screen):
+    font = pygame.font.SysFont(None, 36)
+    time = font.render(f"{int(time / 1000)}", True, (225, 225, 225))
+    pygame.draw.rect(screen, (0, 105, 0), (550, 815, 50, 500))
+    pygame.draw.rect(screen, (0, 105, 0), (600, 815, 50, 500))
+    pygame.draw.rect(screen, (0, 105, 0), (650, 815, 50, 500))
+    screen.blit(time, (550, 815))
+    second = font.render(f"seconds", True, (225, 225, 225))
+    screen.blit(second, (585, 815))
+
 def main():
     time = 0
-    screen.fill((225, 225, 225))
+    num_flags = 16
+    font = pygame.font.SysFont(None, 84)
+    screen.fill((0, 105, 0))
 
     flags = Grid()
-    background = generate_puzzle()
+    background = generate_puzzle(num_flags)
     foreground = Grid("")
     foreground.convert_grid()
     foreground.visual_set_up(screen)
@@ -93,28 +112,36 @@ def main():
                 x = int(x / 100)
                 #print(int(x), int(y), background.grid[y][x])
                 mouse_button = pygame.mouse.get_pressed(num_buttons=3)
-                if mouse_button[0] == True:
-                    if first_click == True:
-                        if type(flags.grid[y][x]) == int:
-                            if background.grid[y][x].displayed == False:
-                                foreground.delete(background, flags, screen, (y, x))
-                                if type(background.grid[y][x]) == Bomb:
-                                    lost_game()
-                    elif first_click == False:
-                        while True: 
-                            if type(background.grid[y][x]) == Bomb: 
-                                background = generate_puzzle()
+                try: 
+                    if mouse_button[0] == True:
+                        if first_click == True:
+                            if type(flags.grid[y][x]) == int:
+                                if background.grid[y][x].displayed == False:
+                                    foreground.delete(background, flags, screen, (y, x))
+                                    if type(background.grid[y][x]) == Bomb:
+                                        lost_game(font)
+                        elif first_click == False:
+                            while True: 
+                                if type(background.grid[y][x]) == Bomb: 
+                                    background = generate_puzzle(num_flags)
+                                else:
+                                    foreground.delete(background, flags, screen, (y, x), 1)
+                                    first_click = True
+                                    time = 0
+                                    break
+                    elif mouse_button[2]:
+                        if background.grid[y][x].displayed == False: 
+                            if flags.grid[y][x] == 0:
+                                flags.grid[y][x] = Flag((y, x))
+                                flags.grid[y][x].display(screen)
+                                num_flags -= 1
                             else:
-                                foreground.delete(background, flags, screen, (y, x), 1)
-                                first_click = True
-                                break
-                elif mouse_button[2]:
-                    if background.grid[y][x].displayed == False: 
-                        if flags.grid[y][x] == 0:
-                            flags.grid[y][x] = Flag((y, x))
-                            flags.grid[y][x].display(screen)
-                        else:
-                            foreground.remove_flag(flags, (y, x), screen)
+                                foreground.remove_flag(flags, (y, x), screen)
+                                num_flags += 1
+                except:
+                    pass
+                finally: 
+                    bottom_bar_flags(num_flags, screen)
         
 
         # background.visual_set_up(screen)
@@ -122,9 +149,11 @@ def main():
 
         
         #foreground.visual_set_up(screen)
+        if first_click == True:
+            bottom_bar_time(time, screen)
 
         if check_win(background, flags):
-            won_game(time)
+            won_game(time, font)
             break
 
         pygame.display.update()
