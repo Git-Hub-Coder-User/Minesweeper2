0a6c822d5abc6f089db0f61ab8d6fa47f51951c9
Git-Hub-Coder-user <elsa.schultheiss@gmail.com>
2024-12-16 17:02:21 -0700

    Improved visuals!

--- main.py
+++ main.py
@@ -76,11 +76,11 @@ def bottom_bar_flags(flags, screen):
 
 def bottom_bar_time(time, screen):
     font = pygame.font.SysFont(None, 36)
-    time = font.render(f"{int(time / 1000)}", True, (225, 225, 225))
+    time = font.render(f"{int(time / 1000)} seconds", True, (225, 225, 225))
     pygame.draw.rect(screen, (0, 105, 0), (550, 815, 500, 500))
     screen.blit(time, (550, 815))
-    second = font.render(f"seconds", True, (225, 225, 225))
-    screen.blit(second, (585, 815))
+    #second = font.render(f"seconds", True, (225, 225, 225))
+    #screen.blit(second, (585, 815))
 
 def main():
     time = 0
